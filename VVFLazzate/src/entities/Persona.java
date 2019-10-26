122b473d4eca72e4113bcb912ce0db45fb2dff68
piero <piero@DESKTOP-47L0RBU.homenet.telecomitalia.it>
2019-10-26 09:11:46 +0200

    Costruttore

--- VVFLazzate/src/entities/Persona.java
+++ VVFLazzate/src/entities/Persona.java
@@ -6,5 +6,15 @@ public class Persona {
 
 	String nome, cognome, genere, datanascita, comuneRes, tel, mail;
 	
+		// COSTRUTTORI
+
+	public Persona(String nome, String cognome, String datanascita, String comuneRes, String tel, String mail) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.datanascita = datanascita;
+		this.comuneRes = comuneRes;
+		this.tel = tel;
+		this.mail = mail;
+	}
 	
 }
