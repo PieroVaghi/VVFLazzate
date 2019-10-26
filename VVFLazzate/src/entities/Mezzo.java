7ef1f2217c7c1b92dd69d5bbf01dc7259e4ff38d
piero <piero@DESKTOP-47L0RBU.homenet.telecomitalia.it>
2019-10-26 18:00:21 +0200

    Tutto Su Master

---
+++ VVFLazzate/src/entities/Mezzo.java
@@ -0,0 +1,100 @@
+package entities;
+
+public class Mezzo {
+	
+	private String nome, sigla, targa;
+	private int posti;
+	
+	private static  String charNotLetters = ",.;:-_|!£$%&/()=?^*+€°§";
+	
+	public Mezzo(String nome, String sigla, String targa, int posti) {
+		this.nome = nome;
+		this.sigla = sigla;
+		this.targa = targa;
+		this.posti = posti;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if(isNomeValido(nome))
+			this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		if(isSiglaValida(sigla))
+			this.sigla = sigla;
+	}
+
+	public String getTarga() {
+		return targa;
+	}
+
+	public void setTarga(String targa) {
+		if(isTargaValida(targa))
+			this.targa = targa;
+	}
+
+	public int getPosti() {
+		return posti;
+	}
+
+	public void setPosti(int posti) {
+		if(isPostiValidi(posti))
+			this.posti = posti;
+	}
+	
+	static boolean isValido(String[] riga) {
+		return 	isNomeValido(riga[0])	&&
+				isSiglaValida(riga[1])	&&
+				isTargaValida(riga[2])	&&
+				isPostiValidi(Integer.parseInt(riga[3]));
+	}
+	
+	private static boolean isNomeValido(String n) {
+		boolean ris = true;
+		if(!n.isEmpty()) {
+			String[] numeri = charNotLetters.split("");
+			for( String s : numeri)
+				if(n.indexOf(s) >=0)
+					return false;
+		}
+		else 
+			ris = false;
+		return ris;	
+	}
+
+	private static boolean isSiglaValida(String n) {
+		boolean ris = true;
+		if(!n.isEmpty()) {
+			String[] numeri = charNotLetters.split("");
+			for( String s : numeri)
+				if(n.indexOf(s) >=0)
+					return false;
+		}
+		else 
+			ris = false;
+		return ris;	
+	}
+
+	private static boolean isTargaValida(String targa) {
+		return targa.length() == 7 && targa.substring(0,2).equalsIgnoreCase("vf");
+	}
+
+	private static boolean isPostiValidi(int posti) {
+		return posti <= 6 && posti >= 2;
+	}
+
+	@Override
+	public String toString() {
+		return (nome != null ? "Nome: " + nome + ", \n" : "") + (sigla != null ? "Sigla: " + sigla + ", \n" : "")
+				+ (targa != null ? "Targa: " + targa + ", \n" : "") + "Posti: " + posti;
+	}	
+
+}
---
+++ VVFLazzate/src/entities/Vigile.java
@@ -0,0 +1,100 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Vigile extends Persona {
+	
+	private String grado;
+	private int patente, annoDecreto;
+	private List<String> qualifiche = new ArrayList<String>();
+	
+	static String[] qualificheVal = {"TPSS","SAF","NBCR","ATP"};
+	static String[] gradiVal = {"VV","CSV"};
+	
+	// COSTRUTTORE
+	
+	public Vigile(String nome, String cognome, String datanascita, String comuneRes, String tel, String mail,
+			String grado, int patente, int annoDecreto, String qualifiche) {
+		super(nome, cognome, datanascita, comuneRes, tel, mail);
+		this.grado = grado;
+		this.patente = patente;
+		this.annoDecreto = annoDecreto;
+		setQualifiche(qualifiche);
+	}
+	
+	// GET - SET
+	
+	public String getGrado() {
+		return grado;
+	}
+
+	public void setGrado(String grado) {
+		if(isGradoValido(grado))
+			this.grado = grado;
+	}
+
+	public int getPatente() {
+		return patente;
+	}
+
+	public void setPatente(int patente) {
+		if(isPatenteValida(patente))
+			this.patente = patente;
+	}
+
+	public int getAnnoDecreto() {
+		return annoDecreto;
+	}
+
+	public void setAnnoDecreto(int annoDecreto) {
+		if(isAnnoDecValido(annoDecreto))
+			this.annoDecreto = annoDecreto;
+	}
+
+	public List<String> getQualifiche() {
+		return qualifiche;
+	}
+
+	public void setQualifiche(String qualifiche) {
+		List<String> qual = Arrays.asList(qualifiche);
+		for(String s : qual)
+			for(String z: qualificheVal)
+				if(s.equalsIgnoreCase(z))
+					this.qualifiche.add(s);
+	}
+	
+	// VALIDAZIONE
+	
+	public static boolean isValido(String[] parti) {	//TODO DA SISTEMARE TUTTE LE PARTI DI PARTI
+		return  Persona.isValido(parti) 	&&
+				isGradoValido(parti[12]) 	&&
+				isPatenteValida(Integer.parseInt(parti[5]))	&&
+				isAnnoDecValido(Integer.parseInt(parti[2]))	;
+	}
+
+	private static boolean isAnnoDecValido(int annoD) {
+		return Persona.isAnnoGiusto(annoD);
+	}
+
+	private static boolean isPatenteValida(int patente) {
+		return patente >= 0 && patente <= 3;
+	}
+
+	private static boolean isGradoValido(String grado) {
+		for(String s : gradiVal)
+			if(s.equalsIgnoreCase(grado))
+				return true;
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + (grado != null ? "Grado: " + grado + ", \n" : "") + "Patente: " + patente + "° grado, \nAnnoDecreto: "
+				+ annoDecreto + ", \n" + (qualifiche != null ? "Qualifiche: " + qualifiche : "");
+	}
+	
+	
+	
+}
