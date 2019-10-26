9aab65599520cfcb35daad9600b8460fc041dd1a
piero <piero@DESKTOP-47L0RBU.homenet.telecomitalia.it>
2019-10-26 22:47:21 +0200

    Creata classe aggregatrice Distaccamnto

---
+++ VVFLazzate/src/aggregatore/Distaccamento.java
@@ -0,0 +1,16 @@
+package aggregatore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Intervento;
+import entities.Mezzo;
+import entities.Persona;
+
+public class Distaccamento {
+	
+	private List<Persona> personale = new ArrayList<Persona>();
+	private List<Mezzo> mezzi = new ArrayList<Mezzo>();
+	private List<Intervento> interventi = new ArrayList<Intervento>();
+	
+}
