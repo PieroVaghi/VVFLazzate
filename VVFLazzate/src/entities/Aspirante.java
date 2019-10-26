756d99902c65ce58ec8dd4deac166f7aac531915
piero <piero@DESKTOP-47L0RBU.homenet.telecomitalia.it>
2019-10-26 10:31:05 +0200

    Creata classe Aspirante, costruttore, get - set e validazione

---
+++ VVFLazzate/src/entities/Aspirante.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class Aspirante extends Persona {
+	
+	private boolean decretato;
+
+	public Aspirante(String nome, String cognome, String datanascita, String comuneRes, String tel, String mail,
+			String decretato) {
+		super(nome, cognome, datanascita, comuneRes, tel, mail);
+		setDecretato(decretato);
+	}
+
+	public boolean isDecretato() {
+		return decretato;
+	}
+
+	public void setDecretato(String decretato) {
+		if(isDecretatoValid(decretato) && decretato.equalsIgnoreCase("si"))
+			this.decretato = true;
+		else
+			this.decretato = false;
+	}
+
+	private boolean isDecretatoValid(String decretato) {
+		if(decretato.equalsIgnoreCase("si") || decretato.equalsIgnoreCase("no"))
+			return true;
+		return false;
+	}
+	
+	
+
+}
