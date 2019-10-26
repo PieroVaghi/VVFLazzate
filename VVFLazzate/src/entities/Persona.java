2823a7a409424ae9871f6d0b25dd7711ec110c7f
piero <piero@DESKTOP-47L0RBU.homenet.telecomitalia.it>
2019-10-26 09:12:42 +0200

    Get - Set

--- VVFLazzate/src/entities/Persona.java
+++ VVFLazzate/src/entities/Persona.java
@@ -17,4 +17,54 @@ public class Persona {
 		this.mail = mail;
 	}
 	
+	// GET-SET
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getDatanascita() {
+		return datanascita;
+	}
+
+	public void setDatanascita(String datanascita) {
+		this.datanascita = datanascita;
+	}
+
+	public String getComuneRes() {
+		return comuneRes;
+	}
+
+	public void setComuneRes(String comuneRes) {
+		this.comuneRes = comuneRes;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
 }
