3e84edc107aa58bee257af08dae44ce2a8d8aee8
piero <piero@DESKTOP-47L0RBU.homenet.telecomitalia.it>
2019-10-26 09:40:07 +0200

    Get e Set

--- VVFLazzate/src/entities/Vigile.java
+++ VVFLazzate/src/entities/Vigile.java
@@ -8,6 +8,8 @@ public class Vigile extends Persona {
 	int patente, annoDecreto;
 	List<String> qualifiche;
 	
+	// COSTRUTTORE
+	
 	public Vigile(String nome, String cognome, String datanascita, String comuneRes, String tel, String mail,
 			String grado, int patente, int annoDecreto, List<String> qualifiche) {
 		super(nome, cognome, datanascita, comuneRes, tel, mail);
@@ -17,4 +19,39 @@ public class Vigile extends Persona {
 		this.qualifiche = qualifiche;
 	}
 	
+	// GET - SET
+	
+	public String getGrado() {
+		return grado;
+	}
+
+	public void setGrado(String grado) {
+		this.grado = grado;
+	}
+
+	public int getPatente() {
+		return patente;
+	}
+
+	public void setPatente(int patente) {
+		this.patente = patente;
+	}
+
+	public int getAnnoDecreto() {
+		return annoDecreto;
+	}
+
+	public void setAnnoDecreto(int annoDecreto) {
+		this.annoDecreto = annoDecreto;
+	}
+
+	public List<String> getQualifiche() {
+		return qualifiche;
+	}
+
+	public void setQualifiche(List<String> qualifiche) {
+		this.qualifiche = qualifiche;
+	}
+	
+	//
 }
